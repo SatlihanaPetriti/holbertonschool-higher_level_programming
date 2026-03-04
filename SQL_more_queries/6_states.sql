d509bb07de593d5f4f2976f074bca63b6e2714c9
satlihana_p <satlihanapetriti@gmail.com>
2026-03-04 13:08:42 +0100

    6 states error check

--- SQL_more_queries/6_states.sql
+++ SQL_more_queries/6_states.sql
@@ -1,3 +1,4 @@
--- script states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; USE hbtn_0d_usa;
+-- script states for the database hbtn_0d_usa.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,name VARCHAR(256) NOT NULL);
